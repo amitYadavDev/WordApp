dfa5db03e033146c006c1eea1bdbf2d5605b88b4
amitYadavDev <amit8955461017@gmail.com>
2024-02-10 23:09:44 +0530

    ui implemented

---
+++ app/src/main/java/com/amitapps/wordapp/viewmodel/WordViewModel.kt
@@ -0,0 +1,37 @@
+package com.amitapps.wordapp.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.amitapps.wordapp.data.model.Word
+import com.amitapps.wordapp.repository.WordRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WordViewModel (private val repository: WordRepository): ViewModel() {
+
+    val allWords: LiveData<List<Word>> = repository.allWords as LiveData<List<Word>>
+
+    /**
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(word: Word) {
+        CoroutineScope(Dispatchers.IO).launch {
+                repository.insert(word)
+        }
+    }
+}
+
+
+//created the ViewModel and implemented a ViewModelProvider.Factory that gets as a parameter
+//the dependencies needed to create WordViewModel: the WordRepository.
+class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WordViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
