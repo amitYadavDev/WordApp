eb84cb46559f7ebe4ff88d4365175be1f159c1fc
amitYadavDev <amit8955461017@gmail.com>
2024-02-11 07:02:16 +0530

    database impl

--- app/src/main/java/com/amitapps/wordapp/data/model/Word.kt
+++ app/src/main/java/com/amitapps/wordapp/data/model/Word.kt
@@ -6,6 +6,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "word_table")
 data class Word(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int = 0,
     val word: String
 )
